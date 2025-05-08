f8d80523cbc630e054de9e626184b64c8589d00e
owalishawon <owali.shawon@gmail.com>
2025-05-08 12:03:07 +0600

    Update app.module.ts connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ServicesModule } from './services/services.module';
+import { Service } from './services/entities/service.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'pass',
+      database: 'booking-services',
+      entities: [Service],
+      synchronize: true,
+    }),
+    ServicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
