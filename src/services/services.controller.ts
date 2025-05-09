823f302d06f9ef780baf4d8d713d3b11a1661e22
OwaliShawon <124124094+owali-softic@users.noreply.github.com>
2025-05-09 20:56:15 +0600

    Update services guard define

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -15,12 +15,13 @@ import { UpdateServiceDto } from './dto/update-service.dto';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
-@UseGuards(JwtAuthGuard)
-@ApiBearerAuth()
+
 @Controller('services')
 export class ServicesController {
   constructor(private readonly servicesService: ServicesService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Post()
   create(@Body() createServiceDto: CreateServiceDto) {
     return this.servicesService.create(createServiceDto);
@@ -36,11 +37,15 @@ export class ServicesController {
     return this.servicesService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateServiceDto: UpdateServiceDto) {
     return this.servicesService.update(+id, updateServiceDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.servicesService.remove(+id);
