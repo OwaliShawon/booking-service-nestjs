02e68821edd1586632782b03d7db82f0887bc43c
OwaliShawon <124124094+owali-softic@users.noreply.github.com>
2025-05-09 16:44:43 +0600

    Add create booking test

--- src/bookings/bookings.controller.spec.ts
+++ src/bookings/bookings.controller.spec.ts
@@ -1,20 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Booking } from './entities/booking.entity';
+import { Service } from '../services/entities/service.entity';
 
 describe('BookingsController', () => {
   let controller: BookingsController;
+  let service: BookingsService;
 
   beforeEach(async () => {
+    const mockBookingRepository = {};
+    const mockServiceRepository = {};
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookingsController],
-      providers: [BookingsService],
+      providers: [
+        BookingsService,
+        {
+          provide: getRepositoryToken(Booking),
+          useValue: mockBookingRepository,
+        },
+        {
+          provide: getRepositoryToken(Service),
+          useValue: mockServiceRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<BookingsController>(BookingsController);
+    service = module.get<BookingsService>(BookingsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a booking and return status 201', async () => {
+      const createBookingDto = {
+        serviceId: 1,
+        customerName: 'Owali Shawon',
+        phone: '01521320960',
+        schedule: new Date('2025-05-10T10:00:00.000Z'),
+      };
+
+      const mockResult = {
+        id: 1,
+        ...createBookingDto,
+        status: 'Pending',
+        service: {
+          id: 1,
+          name: 'Service Name',
+          category: 'Service Name',
+          price: 10.1,
+          description: 'Service Description',
+          createdAt: new Date('2025-05-09T01:02:48.179Z'),
+          updatedAt: new Date('2025-05-09T01:02:48.179Z'),
+        },
+      };
+
+      jest.spyOn(service, 'create').mockResolvedValue(mockResult);
+
+      const result = await controller.create(createBookingDto);
+
+      expect(service.create).toHaveBeenCalledWith(createBookingDto);
+      expect(result).toEqual(mockResult);
+    });
   });
 });
